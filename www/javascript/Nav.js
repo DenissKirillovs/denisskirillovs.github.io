7bd77d9c23b57cb48581d5d9aa4629c5a127989e
DenissKirillovs <deniss.kirillovs@gmail.com>
2020-11-10 16:14:36 +0000

    Revamped the entire website - kept old stuff for future reference, entirely new idea since the spec is now added 10/11/2020. Added Navigation bar/started to work on mobile support via hamburger menu (Version: 1.0)

---
+++ www/javascript/Nav.js
@@ -0,0 +1,12 @@
+function myFunction(id)
+{
+    var x = document.getElementById(id);
+    if (x.style.display ==="block") 
+    {
+        x.style.display = "none";
+    }
+    else 
+    {
+        x.style.display = "block";
+    }
+}
