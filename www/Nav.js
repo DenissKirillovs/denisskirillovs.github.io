6311fa16cc3da7d93d0443d02e1575e44f686cb3
DenissKirillovs <deniss.kirillovs@gmail.com>
2020-11-25 22:54:01 +0000

    Chose a different colour for the website, hamburger header fully working and smooth click scrolling is added

---
+++ www/Nav.js
@@ -0,0 +1,65 @@
+function myFunction()
+{
+    var x = document.getElementById("subMyName");
+    if (x.style.display ==="block") 
+    {
+        x.style.display = "none";
+    }
+    else 
+    {
+        x.style.display = "block";
+    }
+
+
+    var x = document.getElementById("subAboutMe");
+    if (x.style.display ==="block") 
+    {
+        x.style.display = "none";
+    }
+    else 
+    {
+        x.style.display = "block";
+    }
+
+    var x = document.getElementById("subProjects");
+    if (x.style.display ==="block") 
+    {
+        x.style.display = "none";
+    }
+    else 
+    {
+        x.style.display = "block";
+    }
+
+    var x = document.getElementById("subPersonalWork");
+    if (x.style.display ==="block") 
+    {
+        x.style.display = "none";
+    }
+    else 
+    {
+        x.style.display = "block";
+    }
+
+    var x = document.getElementById("subContact");
+    if (x.style.display ==="block") 
+    {
+        x.style.display = "none";
+    }
+    else 
+    {
+        x.style.display = "block";
+    }
+
+    var x = document.getElementById("subResume");
+    if (x.style.display ==="block") 
+    {
+        x.style.display = "none";
+    }
+    else 
+    {
+        x.style.display = "block";
+    }
+
+
+}
