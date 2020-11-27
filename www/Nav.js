6918ad2465e4a78cf3f89da8d975108bbb474258
DenissKirillovs <deniss.kirillovs@gmail.com>
2020-11-27 01:01:57 +0000

    Slideshow created, About me Section created, fixed some abnormal bugs with mobile to fullscreen resizing

--- www/Nav.js
+++ www/Nav.js
@@ -1,4 +1,4 @@
-function myFunction()
+function hamburgerMenu()
 {
     var x = document.getElementById("subMyName");
     if (x.style.display ==="block") 
@@ -63,3 +63,38 @@ function myFunction()
 
 
 }
+
+
+
+// START OF SLIDESHOW JS
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous buttons
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image buttons
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// END OF SLIDESHOW JS
